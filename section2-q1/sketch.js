1f16b6013c124a480ba458add9ab21db7710f077
cimerci <ballethazuki@gmail.com>
2023-10-10 13:58:57 +0900

    update section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,22 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  createCanvas(500,500);
+  noFill()
+  for(let i = 0; i < 5; i++){
+  let x = 60 + i * 10
+  stroke (255,0,0)
+  ellipse(100,100,x,x)
+  //  ellipse (100,100,100,100)
+  //  ellipse(100,100,90,90)
+  //  ellipse(100,100,80,80)
+  //  ellipse(100,100,70,70)
+  //  ellipse(100,100,60,60)
+  //  ellipse (100,100,50,50)
+  let y = 10 + i * 10 
+  stroke (0,0,255)
+  ellipse(100,100,y,y)
+    // ellipse (100,100,40,40)
+    // ellipse(100,100,10,10)
     // BLANK[1]
   }
 }
