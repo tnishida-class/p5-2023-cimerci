b46e1a200ca566b0f1f28dc1bfc8c81254592178
cimerci <ballethazuki@gmail.com>
2023-11-05 07:32:31 +0900

    update section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,7 +7,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(20, 100); // 20以上100未満のランダムな数を代入
   }
 
   // 横線を引く
@@ -15,10 +15,20 @@ function setup(){
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
 
   // ここからが本番
-  fill(0);
+  fill(0); //
   const dx = width / scores.length;
   let px, py; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
     // BLANK[1]
+    let x = dx * i + dx / 2;
+    let y = scores[i];
+
+    ellipse(x, y, 7);
+
+    if (i > 0) {
+    line(px,py,x,y);
+    }
+    px = x;
+    py = y;
   }
 }
