bfcb0968184aa544561279322af5e8382077a39c
cimerci <ballethazuki@gmail.com>
2023-11-06 20:21:35 +0900

    2nd update section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -18,7 +18,33 @@ function setup() {
       if(((i+j)%2 == 1)&&(j>4)){
           fill (0)
           ellipse(x+size/2,y+size/2,20)}
-}
+    }
   }
-
 }
+
+//2回目制作
+// function setup() {
+//   createCanvas(400, 400);
+// }
+
+// function draw() {
+//   dx = width / 8
+//   noStroke();
+//   for(let i = 0; i<8; i++){
+//     for(let s = 0; s < 8; s++){
+//       // line(0,dx * i,width,dx*i)
+//       // line(dx * s, 0, dx *s, width)
+//       if((i + s)%2 == 1){
+//         fill(100);
+//         rect(dx * s, dx * i, dx);
+//         if(i<3){
+//           fill(255,0,0);
+//         }
+//         if(i>4) {
+//           fill(0);
+//         }
+//           ellipse(dx*s + dx/2, dx*i+ dx/2, dx - 1);
+//       }
+//     }
+//   }
+// }
